fa250643c72579ef0d842d3342672faf4b96c048
XinyuGaoUmich <xinyugaoumich@gmail.com>
2018-03-31 18:00:59 -0400

    big table

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"strconv"
 	"github.com/pborman/uuid"
 	"reflect"
+	"context"
+      	"cloud.google.com/go/bigtable"
 
 )
 
@@ -77,6 +79,30 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	id := uuid.New()
 	// Save to ES.
 	saveToES(&p, id)
+	  ctx := context.Background()
+      // you must update project name here
+      bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+      if err != nil {
+             panic(err)
+             return
+      }
+
+      // TODO (student questions) save Post into BT as well
+	  tbl := bt_client.Open("post")
+      mut := bigtable.NewMutation()
+      t := bigtable.Now()
+
+      mut.Set("post", "user", t, []byte(p.User))
+      mut.Set("post", "message", t, []byte(p.Message))
+      mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+      mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+      err = tbl.Apply(ctx, id, mut)
+      if err != nil {
+             panic(err)
+             return
+      }
+      fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 
 }
 
@@ -114,8 +140,8 @@ const (
 	TYPE = "post"
 	DISTANCE = "200km"
 	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
+	PROJECT_ID = "shaped-glazing-199302"
+	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://35.184.85.252:9200"
 )
